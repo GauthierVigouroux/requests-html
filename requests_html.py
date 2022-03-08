841253891117edbeb25c76dba7244e8be4c17a56
GauthierVigouroux <gauthier.vigouroux@ecole-navale.fr>
2022-03-08 16:58:46 +0100

    08032022

--- requests_html.py
+++ requests_html.py
@@ -297,7 +297,7 @@ class BaseParser:
 
                 try:
                     href = link.attrs['href'].strip()
-                    if href and not (href.startswith('#') and self.skip_anchors) and not href.startswith(('javascript:', 'mailto:')):
+                    if href: #and not (href.startswith('#') and self.skip_anchors) and not href.startswith(('javascript:', 'mailto:')):
                         yield href
                 except KeyError:
                     pass
